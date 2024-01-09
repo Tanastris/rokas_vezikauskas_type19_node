43e74bfa30a152a7a261f0f3c13d9ab4b65c905e
RokasV <vezikauskas.r@gmail.com>
2024-01-09 15:00:59 +0200

    Get all orders by user id

--- src/db/routes/ordersRoutes.js
+++ src/db/routes/ordersRoutes.js
@@ -31,5 +31,30 @@ ordersRouter.get('/api/orders', async (req, res) => {
 
 // 7.3. GET /api/orders/:id - gauti užsakymą pagal id
 
+ordersRouter.get('/api/orders/:id', async (req, res) => {
+  const itemId = +req.params.id;
+  const sql = 'SELECT * FROM orders WHERE order_id=?';
+  const [rows, error] = await dbQueryWithData(sql, [itemId]);
+  if (error) {
+    res.status(500).json({ error: 'Internal server error' });
+    return;
+  }
+  res.json(rows[0]);
+});
+
 // 7.4. GET /api/orders/user/user_id-gauti visus užsakymus, priklausančius tam tikram vartotojui. gauta informacija turi būti su vartojo vardu ir prekės pavadinimu bei vieneto kaina
+
+ordersRouter.get('/api/orders/user/:user_id', async (req, res) => {
+  const userId = +req.params.user_id;
+  const sql =
+    'SELECT users.user_name, shop_items.shop_item_name, shop_items.shop_item_price FROM orders JOIN users ON users.user_id = orders.user_id JOIN shop_items ON shop_items.shop_item_id = orders.shop_item_id WHERE orders.user_id=?';
+
+  const [rows, error] = await dbQueryWithData(sql, [userId]);
+  if (error) {
+    res.status(500).json({ error: 'Internal server error' });
+    return;
+  }
+  res.json(rows);
+});
+
 module.exports = ordersRouter;
--- src/db/routes/shopRoutes.js
+++ src/db/routes/shopRoutes.js
@@ -15,7 +15,8 @@ shopRouter.post('/api/shop_items', async (req, res) => {
     shop_item_image,
     item_type_id,
   } = req.body;
-  const sql = 'INSERT INTO shop_items (shop_item_name, shop_item_price, shop_item_description, shop_item_image, item_type_id) VALUES (?, ?, ?, ?, ?)';
+  const sql =
+    'INSERT INTO shop_items (shop_item_name, shop_item_price, shop_item_description, shop_item_image, item_type_id) VALUES (?, ?, ?, ?, ?)';
   const argArr = [
     shop_item_name,
     shop_item_price,
@@ -33,7 +34,8 @@ shopRouter.post('/api/shop_items', async (req, res) => {
 
 // 5.2. GET /api/shop_items - gauti visas parduotuvės prekes
 shopRouter.get('/api/shop_items', async (req, res) => {
-  const sql = 'SELECT shop_item_id, shop_item_name, shop_item_price, shop_item_description, shop_item_image, item_type_id FROM shop_items WHERE isDeleted=0';
+  const sql =
+    'SELECT shop_item_id, shop_item_name, shop_item_price, shop_item_description, shop_item_image, item_type_id FROM shop_items WHERE isDeleted=0';
   const [rows, error] = await dbQueryWithData(sql);
   if (error) {
     res.status(500).json({ error: 'Internal server error' });
@@ -51,7 +53,7 @@ shopRouter.get('/api/shop_items/:id', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
     return;
   }
-  res.json(rows[0]);
+  res.json(rows);
 });
 
 // 5.4. DELETE/api/shop_items/:id - ištrinti parduotuvės prekę pagal id
